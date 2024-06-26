4f57a16f8381691443685ee962138afcb42569c5
Aditya Goel <adityagoelgithub0320@gmail.com>
2024-06-27 02:25:50 +0530

    deployed url added in codebase

--- frontend/src/assets/FrontendUtils.jsx
+++ frontend/src/assets/FrontendUtils.jsx
@@ -1,11 +1,2 @@
-export const backendUrl = "http://localhost:9000"
-// export const backendUrl = "https://pizza-website-6np8.onrender.com"
-export const AppName = "PIZZA PARADISE"
-
-
-
-// assets/FrontendUtils.js
-
-// Assuming your images are in a 'public/images' directory
-export const imageUrl = (imageName) => `/images/pizzaBackground.jpeg`;
+export const backendUrl = "https://project-j4pu.onrender.com"
 
